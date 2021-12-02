986140495bae4375ff38dba718d1329ff256a76a
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-12-02 13:04:33 +0000

    setup run on BP1

--- run/run.sh
+++ run/run.sh
@@ -6,9 +6,12 @@
 #SBATCH --ntasks-per-node=5
 #SBATCH --time=0-01:00:00
 
+pwd
+
 export MKL_THREADING_LAYER=TBB
 export OMP_NUM_THREADS=1
 
 for i in ../xyzs/*.xyz; do
-	echo ${i};
+	file_name=$(basename ${i})
+	~/.local/src/Qcore/release/bin/qcore -s "res := xtb(structure(file = '${i}') model='chlorophyll') print(load = res variable = shell_resolved_partial_charges)" > ~/bchla_initial_guess/outputs/${file_name/.xyz/.out}
 done
